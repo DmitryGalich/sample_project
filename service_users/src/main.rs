80ac24460e9b039835036dbcec081e2eb1198a0c
d.galich <d.galich@iva.ru>
2026-08-20 21:52:16 +0300

    rust debugging

--- service_users/src/main.rs
+++ service_users/src/main.rs
@@ -1,9 +1,13 @@
-use sqlx::PgPool;
-use tonic::transport::Server;
+// use sqlx::PgPool;
+// use tonic::transport::Server;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:50051".parse()?;
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let addr = "0.0.0.0:50051".parse()?;
 
-    Ok(())
+//     Ok(())
+// }
+
+fn main() {
+    println!("Hello, World!");
 }
