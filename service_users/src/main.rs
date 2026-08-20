fa7a6e20feb8682bd68d9457cd1dc774e61c6d1c
d.galich <d.galich@iva.ru>
2026-08-20 19:33:23 +0300

    rust container is up

---
+++ service_users/src/main.rs
@@ -0,0 +1,9 @@
+use sqlx::PgPool;
+use tonic::transport::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50051".parse()?;
+
+    Ok(())
+}
