24a3b50517558d13fcb1ec5ce78ae24f8173a732
d.galich <d.galich@iva.ru>
2026-08-22 21:36:36 +0300

    nginx incorrect request 404, rust http

---
+++ gateway/README.md
@@ -0,0 +1,9 @@
+* Runtime reset config
+```
+nginx -s reload
+```
+
+* Print current config
+```
+nginx -T
+```
--- service_users/src/main.rs
+++ service_users/src/main.rs
@@ -1,10 +1,28 @@
 use std::env;
 
-fn main() {
-    let key = "EXPOSED_ADDR";
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-    match env::var(key) {
-        Ok(val) => println!("{key} is set to: {val}"),
-        Err(e) => println!("Couldn't read {key}: {e}"),
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let exposed_addr = env::var("EXPOSED_ADDR")?;
+ 
+    let listener = TcpListener::bind(exposed_addr.clone()).await?;
+    println!("Raw TCP/HTTP Server running on {exposed_addr}...");
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
+            
+            if let Ok(n) = socket.read(&mut buffer).await {
+                if n == 0 { return; }
+
+                let response = "HTTP/1.1 200 OK\r\nContent-Length: 22\r\nContent-Type: text/plain\r\n\r\nHello from raw Tokio!";
+                
+                let _ = socket.write_all(response.as_bytes()).await;
+            }
+        });
     }
 }
