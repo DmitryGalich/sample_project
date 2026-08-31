bcde76c13c132d074d103554c297677a2d8c450e
d.galich <d.galich@iva.ru>
2026-08-31 23:38:15 +0300

    migrations upgrade

---
+++ projects/migrations/02_seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO projects (
+    id, 
+    owner_id,
+    title,
+    description
+) VALUES 
+(
+    '01000000-0000-0000-0000-000000000000',
+    '00000000-0000-0000-0000-000000000000',
+    'ivanov ivan project',
+    'ivanov ivan project desc'
+),
+(
+    '11000000-0000-0000-0000-000000000000',
+    '01000000-0000-0000-0000-000000000000',
+    'petrov petr project',
+    'petrov petr project desc'
+);
--- users/migrations/02_seed.sql
+++ users/migrations/02_seed.sql
@@ -1,12 +1,15 @@
 INSERT INTO users (
+    id,
     email, 
     display_name
 ) VALUES 
 (
+    '00000000-0000-0000-0000-000000000000',
     'ivanov.ivan@gmail.com', 
     'ivanov ivan'
 ),
 (
+    '10000000-0000-0000-0000-000000000000',
     'petrov.petr@gmail.com', 
     'petrov petr'
 );
